5154d08cdd1834f0cac4e8ea3fd74ea6abe4502a
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-12-19 09:34:44 -0500

    Remove console.log

--- frontend/src/misc/InlineSkeletonDisplay.tsx
+++ frontend/src/misc/InlineSkeletonDisplay.tsx
@@ -2,6 +2,5 @@ import { PropsWithChildren } from "react";
 import inlineSkeleton from "./inlineSkeleton";
 
 export default function InlineSkeletonDisplay({ children }: PropsWithChildren) {
-  console.log(children);
   return children !== null ? <b>{children}</b> : inlineSkeleton;
 }
