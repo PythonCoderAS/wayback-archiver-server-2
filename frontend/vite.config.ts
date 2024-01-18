eb65c66e8e6ec40d662ac8b8ba2654ed213c27be
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2024-01-18 18:55:42 -0500

    Fix linting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
 import { env } from "process";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
